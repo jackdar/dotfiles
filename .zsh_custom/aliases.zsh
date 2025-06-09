8a1e9259ee4c30d3a8af64da2771015eb5e483a7
jackdar <jackdarlington2001@gmail.com>
2025-06-10 07:39:47 +1200

    update aliases and karabiner elements config

--- .zsh_custom/aliases.zsh
+++ .zsh_custom/aliases.zsh
@@ -1,13 +1,13 @@
-# alias ll="ls -la"
-alias ls="eza --color=always --git --icons=always --no-filesize --no-time --no-user --no-permissions" # List files in current directory in long list format
-alias ll="eza -la --color=always --git --icons=always --no-user" # List files in current directory in long list format
-alias l="eza --color=always --git --icons=always --no-user" # List files in current directory in long list format
-alias lt="eza --tree --level=3" # List files in current directory in tree format
-alias o="open ." # Open the current directory in Finder
+alias ls="eza --color=always --git --icons=always --no-filesize --no-time --no-user --no-permissions"
+alias ll="eza -la --color=always --git --icons=always --no-user"
+alias l="eza --color=always --git --icons=always --no-user"
+alias tree="eza --tree --level=3"
+alias o="open ."
 alias cd="z"
 alias zz="z -"
-alias vim="nvim"
 alias lg=lazygit
 alias sail='sh $([ -f sail ] && echo sail || echo vendor/bin/sail)'
 alias shadcn="npx shadcn@latest"
 alias nvm="fnm"
+alias vim="nvim"
+alias nvim-old='NVIM_APPNAME="nvim-old" nvim'
